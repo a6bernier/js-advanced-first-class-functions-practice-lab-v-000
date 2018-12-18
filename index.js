5213bb7f4a2d360bcfe65ac21e484e1f9c430c9d
Andrew Bernier <a6bernier@gmail.com>
2018-12-18 18:14:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const logDriverNames = function (drivers) {
   })
 }
 
-const logDriverByHometown = function (drivers){
-  drivers.forEach(function(driver){
-    if (driver.hometown === hometown){
-      console.log(driver.name)
-    })
-}
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    }
+  });
+};
